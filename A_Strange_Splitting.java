eed5c328122924e869854eaa94b55af103d6291f
Imran <iamkhan.nel@gmail.com>
2024-07-26 23:57:31 +0530

    day5

--- A_Strange_Splitting.java
+++ A_Strange_Splitting.java
@@ -6,11 +6,31 @@ public class A_Strange_Splitting{
         while(t-->0)
         {
     int n=sc.nextInt();
-int a[]=new int [n];
-for (int i = 0; i < a.length; i++) {
-    a[i]=sc.nextInt();
-}
-
+    int a[]=new int [n];
+    StringBuilder str=new StringBuilder();
+     for (int i = 0; i < a.length; i++) 
+          a[i]=sc.nextInt();
+          boolean same=true;
+    for (int i = 0; i < a.length; i++) {
+          if(a[i]!= a[0]){
+          same=false;
+          break;
+          }
+    }
+        if(!same) {
+            for (int i = 0; i < a.length; i++) {
+                if (i == 1) { // Check if it's the second element (index 1)
+                    str.append("B");
+                } else {
+                    str.append("R");
+                }
+            }
+            System.out.println("YES");
+            // Print the pattern
+            System.out.println(str.toString());
+   }
+else 
+System.out.println("NO");
         }
         sc.close();
        }
