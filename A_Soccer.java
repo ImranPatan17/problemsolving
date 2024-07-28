8c0e99e1ae26ef5d9cdcdbcf37db018bbb54d42c
Imran <iamkhan.nel@gmail.com>
2024-07-29 00:23:58 +0530

    day7

--- A_Soccer.java
+++ A_Soccer.java
@@ -3,30 +3,9 @@ public class A_Soccer{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int t=sc.nextInt();
-        while(t-->0)
-        {
-    int []x=new int[2];
-    int []y=new int[2];
-    x[0]=sc.nextInt();
-    y[0]=sc.nextInt();
-    x[1]=sc.nextInt();
-    y[1]=sc.nextInt();
-    int numPoints = 100;
-
-    // Check for equality at each intermediate point
-    boolean foundEqual = false;
-    for (int i = 0; i <= numPoints; i++) {
-        double ti = (double) i / numPoints;
-        double xValue = x[0] + ti * (x[1] - x[0]);
-        double yValue =  + ti * (y[1] - y[0]);
-        if (xValue == yValue) {
-            System.out.println("NO");
-            foundEqual = true;
+        while(t-->0){
+            int x1=sc.nextInt(),y1=sc.nextInt(),x2=sc.nextInt(),y2=sc.nextInt();
+            System.out.println((x1<y1&&x2>=y1&&x2>=y2)||(y1<x1&&y2>=x1&&y2>=x2)?"NO":"YES");
         }
-    }
-    if (!foundEqual)
-    System.out.println("YES");
-
         }
-       }
-    }
+}
